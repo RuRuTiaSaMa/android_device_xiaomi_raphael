289cf4b395418d7219a6951e65bcd4a1974ab91b
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-02-01 23:33:54 -0200

    raphael: parts: Dirac fixes
    * Pause/play music stream to get effects applied
    * Rename setMusic to setEnabled
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -94,7 +94,10 @@ public class DiracSettingsFragment extends PreferenceFragment implements
     public void onSwitchChanged(Switch switchView, boolean isChecked) {
         mSwitchBar.setChecked(isChecked);
 
-        DiracUtils.setMusic(isChecked);
+        DiracUtils.setEnabled(isChecked);
+        mHifi.setEnabled(isChecked);
+        mHeadsetType.setEnabled(isChecked);
+        mPreset.setEnabled(isChecked);
 
         if (!DiracUtils.getHifiMode()) {
             mHeadsetType.setEnabled(isChecked);
