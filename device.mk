777ccdc09cc0177e7e1bce94ca23188a47914ba9
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    Raphael: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- device.mk
+++ device.mk
@@ -420,6 +420,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
+
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor
